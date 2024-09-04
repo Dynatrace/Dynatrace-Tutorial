7b815e5f1b5a406834950c0ee9dd2ee7591246a9
eduardo.campver <eduardo.campver@dynatrace.com>
2024-09-04 17:01:41 +0200

    Update imports.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { AppHeader, Page } from "@dynatrace/strato-components-preview";
+import { AppHeader, Page } from "@dynatrace/strato-components-preview/layouts";
 import { HostList } from "./pages/HostList";
 import { StatusHistory } from "./pages/StatusHistory";
 
