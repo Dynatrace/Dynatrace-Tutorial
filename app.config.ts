e2f949c47bc9ceae54d6d95b33f0ad7066ca7bfa
Coll <daniel.coll@dynatrace.com>
2025-05-05 12:08:56 +0200

    fixup! fixup! fixup! fixup! XECO-16121: Add github actions

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,12 @@
-{
-  "environmentUrl": "https://umsaywsjuo.dev.apps.dynatracelabs.com/",
+import { CliOptions } from "dt-app";
+
+const config: CliOptions = {
+  "environmentUrl": process.env.ENVIRONMENT_URL ?? "https://wkf10640.apps.dynatrace.com/",
   "app": {
     "name": "new-tutorial",
     "version": "0.0.0",
     "description": "A starting project with routing, fetching data, and charting",
-    "id": "my.new.tutorial",
+    "id": "my.new.tutorial.github.actions",
     "scopes": [
       { "name": "storage:buckets:read", "comment": "default template" },
       { "name": "storage:metrics:read", "comment": "default template" },
@@ -15,4 +17,6 @@
       { "name" : "app-settings:objects:read", "comment": "Deploying permission"}
     ]
   }
-}
+};
+
+module.exports = config;
